22c7d1c7f9f4fd480239252a324acf489f7cd421
Alice Robertson <robe23am@dukes.jmu.edu>
2021-11-06 18:01:30 -0400

    38% tests passing

--- src/p4-codegen.c
+++ src/p4-codegen.c
@@ -107,6 +107,18 @@ Operand var_offset (ASTNode* node, Symbol* variable)
 #define EMIT2OP(FORM,OP1,OP2)     ASTNode_emit_insn(node, ILOCInsn_new_2op(FORM,OP1,OP2))
 #define EMIT3OP(FORM,OP1,OP2,OP3) ASTNode_emit_insn(node, ILOCInsn_new_3op(FORM,OP1,OP2,OP3))
 
+/**************************** PRE-VISITOR METHODS ****************************/
+
+void CodeGenVisitor_previsit_funcdecl (NodeVisitor* visitor, ASTNode* node)
+{
+    /* generate a label reference for the epilogue that can be used while
+     * generating the rest of the function (e.g., to be used when generating
+     * code for a "return" statement) */
+    DATA->current_epilogue_jump_label = anonymous_label();
+}
+
+/**************************** POST-VISITOR METHODS ****************************/
+
 void CodeGenVisitor_gen_program (NodeVisitor* visitor, ASTNode* node)
 {
     /*
@@ -121,14 +133,6 @@ void CodeGenVisitor_gen_program (NodeVisitor* visitor, ASTNode* node)
     FOR_EACH(ASTNode*, func, node->program.functions) {
         ASTNode_copy_code(node, func);
     }
-}
-
-void CodeGenVisitor_previsit_funcdecl (NodeVisitor* visitor, ASTNode* node)
-{
-    /* generate a label reference for the epilogue that can be used while
-     * generating the rest of the function (e.g., to be used when generating
-     * code for a "return" statement) */
-    DATA->current_epilogue_jump_label = anonymous_label();
 }
 
 void CodeGenVisitor_gen_funcdecl (NodeVisitor* visitor, ASTNode* node)
@@ -172,25 +176,103 @@ void CodeGenVisitor_postvisit_literal (NodeVisitor* visitor, ASTNode* node) {
 
 void CodeGenVisitor_postvisit_return (NodeVisitor* visitor, ASTNode* node)
 {
+    // create value node for easier handling
     ASTNode* value = node->funcreturn.value;
 
     /* copy code from literal */
     ASTNode_copy_code(node, value);
 
+    // emit the instruction
     EMIT2OP(I2I, ASTNode_get_temp_reg(value), return_register());
 
 }
 
 void CodeGenVisitor_postvisit_binaryop (NodeVisitor* visitor, ASTNode* node)
 {
+    // copy code from the left and right children
     ASTNode_copy_code(node, node->binaryop.left);
     ASTNode_copy_code(node, node->binaryop.right);
 
+    // allocate a register and set it as the tempp register for the op
     Operand reg = virtual_register();
-
     ASTNode_set_temp_reg(node, reg);
-    EMIT3OP(ADD, ASTNode_get_temp_reg(node->binaryop.left), ASTNode_get_temp_reg(node->binaryop.right), reg);
 
+    // set the op for the instruction
+    InsnForm op = NOP;
+    switch (node->binaryop.operator) {
+        case OROP:     op = OR;
+            break;
+        case ANDOP:    op = AND;
+            break;
+        case EQOP:     op = CMP_EQ;
+            break;
+        case NEQOP:    op = CMP_NE;
+            break;
+        case LTOP:     op = CMP_LT;
+            break;
+        case LEOP:     op = CMP_LE;
+            break;
+        case GEOP:     op = CMP_GE;
+            break;
+        case GTOP:     op = CMP_GT;
+            break;
+        case ADDOP:    op = ADD;
+            break;
+        case SUBOP:    op = SUB;
+            break;
+        case MULOP:    op = MULT;
+            break;
+        case DIVOP:    op = DIV;
+            break;
+        case MODOP:    op = DIV; //basically, l / r = d ; d * r = p ; l - p = mod
+            //return;
+    }
+
+    // emit the instruction
+    EMIT3OP(op, ASTNode_get_temp_reg(node->binaryop.left), ASTNode_get_temp_reg(node->binaryop.right), reg);
+}
+
+
+void CodeGenVisitor_postvisit_unaryop (NodeVisitor* visitor, ASTNode* node)
+{
+    // copy code from child
+    ASTNode_copy_code(node, node->unaryop.child);
+
+    // allocate a register and set it as the tempp register for the op
+    Operand reg = virtual_register();
+    ASTNode_set_temp_reg(node, reg);
+
+    // set the op for the instruction
+    InsnForm op = NOP;
+    switch (node->unaryop.operator) {
+        case NEGOP:     op = NEG;
+            break;
+        case NOTOP:     op = NOT;
+            break;
+    }
+
+    // emit the instruction
+    EMIT2OP(op, ASTNode_get_temp_reg(node->unaryop.child), reg);
+}
+
+// this currently does not work, I am confused
+void CodeGenVisitor_postvisit_assignment (NodeVisitor* visitor, ASTNode* node)
+{
+    // copy code from child
+    ASTNode_copy_code(node, node->assignment.value);
+
+    // find base pointer for location
+    Symbol *sym = lookup_symbol(node, (node->assignment.location)->location.name);
+
+    EMIT3OP(STORE_AI, ASTNode_get_temp_reg(node->assignment.value), base_register(), var_offset(node, sym));
+}
+
+// I dont know if this is needed or how it would be needed
+void CodeGenVisitor_postvisit_vardecl (NodeVisitor* visitor, ASTNode* node) 
+{
+    // find base pointer for location
+    // Symbol *sym = lookup_symbol(node, node->vardecl.name);
+    // EMIT1OP(PUSH, var_base(node, sym));
 }
 
 #endif
@@ -202,13 +284,21 @@ InsnList* generate_code (ASTNode* tree)
     NodeVisitor* v = NodeVisitor_new();
     v->data = CodeGenData_new();
     v->dtor = (Destructor)CodeGenData_free;
-    v->postvisit_program     = CodeGenVisitor_gen_program;
+
+    /***** pre-visitors *****/
     v->previsit_funcdecl     = CodeGenVisitor_previsit_funcdecl;
+
+    /***** post-visitors *****/
+    v->postvisit_program     = CodeGenVisitor_gen_program;
     v->postvisit_funcdecl    = CodeGenVisitor_gen_funcdecl;
+    v->postvisit_vardecl     = CodeGenVisitor_postvisit_vardecl;
     v->postvisit_block       = CodeGenVisitor_gen_block;
     v->postvisit_literal     = CodeGenVisitor_postvisit_literal;
     v->postvisit_return      = CodeGenVisitor_postvisit_return; 
     v->postvisit_binaryop    = CodeGenVisitor_postvisit_binaryop;
+    v->postvisit_unaryop     = CodeGenVisitor_postvisit_unaryop;
+    v->postvisit_assignment  = CodeGenVisitor_postvisit_assignment;
+
 
     /* generate code into AST attributes */
     NodeVisitor_traverse_and_free(v, tree);
